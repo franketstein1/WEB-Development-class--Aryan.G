3b57a5034d7358d6043661d27137fb1a9a4a3c05
franketstein1 <aryanghimire016@gmail.com>
2026-04-18 23:44:44 +0545

    Restore assignment and classwork folders on main

---
+++ classwork/script.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 20; i++)
+{
+    if (i==3)
+    {
+        continue;
+    }
+    if (i>=15)
+    {
+        break;
+    }
+    console.log(i);
+}
---
+++ classwork/script2.js
@@ -0,0 +1,6 @@
+let arr1 = ['house', 'cow'];
+let arr2 = ['zebra', 'tiger'];
+
+let result = arr1.concat(arr2);
+
+console.log(result);
